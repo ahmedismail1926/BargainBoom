46fe3f9a51a877dd2909803e81893797bec4d218
Ahmed Ismail <108951693+ahmedismail1926@users.noreply.github.com>
2025-05-03 03:09:51 +0300

    test update for messages

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,7 +3,7 @@ const { check } = require("express-validator");
 const messageController = require("../controllers/messageController");
 const auth = require("../middleware/auth");
 const validation = require("../middleware/validation");
-
+//message latest change at 3:09 am
 const router = express.Router();
 
 router.get("/chat/:userId", auth, messageController.getChatHistory);
